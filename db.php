4c9bacceac128cce90e2df63725767d6ea5eabbd
bookwormslib <bookwormslib2@gmail.com>
2017-10-29 13:18:47 +0530

    commit

--- db.php
+++ db.php
@@ -3,11 +3,12 @@
 $host = getenv('REMOTE_HOST');
 $dbuser = getenv('REMOTE_USER');
 $dbpass = getenv('REMOTE_PASS');
+$db =  getenv('REMOTE_DB');
 
 $con = mysql_connect($host, $dbuser, $dbpass) or  
     die("Could not connect: " . mysql_error()); 
 
-mysql_select_db("online");  
+mysql_select_db($db);  
 
 ?>
 
