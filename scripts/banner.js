f0122e002d34d7f727e65c6a2a700d1c947f6df0
bookwormslib <bookwormslib@users.noreply.github.com>
2021-05-31 12:32:35 +0530

    Add files via upload

---
+++ scripts/banner.js
@@ -0,0 +1,8 @@
+document.write('\
+<div id="leftside"> \
+<h1 style="color:white;font-weight: bold;font-variant: small-caps;">#stories library cafe</h1> \
+</div> \
+<div id="rightside"> \
+<img src="cafe-logo.png" style="width:50%;height:50%;object-fit: contain;" align="right"/> \
+</div> \
+');
