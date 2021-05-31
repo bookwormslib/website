96ec3b8e27fcfb68cd620b25233b0d82bf10d5c8
bookwormslib <bookwormslib@users.noreply.github.com>
2021-05-31 12:21:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -207,7 +207,7 @@ $("#totalspan").text(total);
 <h1>#stories library cafe</h1>
 </div>
 <div id="rightside">
-<img src="cafe-logo.png" style="width:12%;height:12%;object-fit: contain;" align="right"/>
+<img src="cafe-logo.png" style="width:50%;height:50%;object-fit: contain;" align="right"/>
 </div>
 </div>
 
