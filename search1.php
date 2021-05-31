bbb2b9a1973ba6eac96f123a36a532e800f23436
bookwormslib <bookwormslib@users.noreply.github.com>
2021-05-31 10:59:37 +0530

    Update search1.php

--- search1.php
+++ search1.php
@@ -119,12 +119,8 @@ a.tooltip:hover span{display:inline; position:absolute; background:#ffffff; bord
 </style>
 
 <body>
+dddddd
 <?php
-
-php_info();
-ini_set('display_startup_errors', '1');
-ini_set('display_errors', '1');
-error_reporting(-1);
   
 require_once("db.php");
 
